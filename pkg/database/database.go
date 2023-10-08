60b9738195ab55670bbfd2fd6ef947330b1fdfa8
Francisco <frangarcia38@hotmail.com>
2023-10-08 09:03:54 -0600

    fix: silent logger of gorm to prevent confussion about errors

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -5,12 +5,15 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewClient(host, user, password, database, databaseport string) (*gorm.DB, error) {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, database, databaseport)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 
 	if err != nil {
 		return nil, err
