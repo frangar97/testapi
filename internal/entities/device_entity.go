46d918d9fa88f9f5e45a576ff061656f6403c801
Francisco <frangarcia38@hotmail.com>
2023-10-08 19:22:26 -0600

    fix: relation between device and firmware, delete strategy set to cascade

--- internal/entities/device_entity.go
+++ internal/entities/device_entity.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Device struct {
-	ID        string    `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
-	Name      string    `gorm:"not null" json:"name"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string     `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Name      string     `gorm:"not null" json:"name"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	Firmwares []Firmware `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
 }
